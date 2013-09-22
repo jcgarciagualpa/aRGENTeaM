75674a236594be814688367a2819056026f9e2cf
martin <martinrevert@gmail.com>
2013-09-22 16:07:15 -0300

    fixed untracked files

--- aRGENTeaM/gen/ar/com/martinrevert/argenteam/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package ar.com.martinrevert.argenteam;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
