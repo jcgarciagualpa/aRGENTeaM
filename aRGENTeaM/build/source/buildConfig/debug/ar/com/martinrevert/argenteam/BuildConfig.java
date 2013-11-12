3478175d6d28d33f5b5b2ab9ded74e89e40aa6e9
martin <martinrevert@gmail.com>
2013-11-12 08:17:11 -0300

    remove unwanted files

--- aRGENTeaM/build/source/buildConfig/debug/ar/com/martinrevert/argenteam/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ar.com.martinrevert.argenteam;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
